f171712fcbb226b369cb47e5855e0385e2c80bff
Agam Airi <75530266+agamairi@users.noreply.github.com>
2024-02-21 17:43:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', ws => {
   ws.on('message', message => {
+    // whenever new message is recieved, it updates on the terminal and sends the message back as acknowledgement
     console.log('Received: %s', message);
     ws.send(`Echo: ${message}`);
   });
