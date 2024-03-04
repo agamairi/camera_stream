8f50ed13522afb2e73ef3d2b6ab0e6dce8801126
agamairi <agam.airi@gmail.com>
2024-03-04 16:13:23 -0500

    update readme with progress

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log('Received: %s', message);
-    ws.send(`Echo: ${message}`);
+    // ws.send("lol, this is above our message");
+    ws.send(message.toString());
   });
 
   ws.send('Hello! I am your WebSocket server.');
 });
 
 console.log('WebSocket server started on ws://localhost:8080');
+
 
 
 // here I was trying to get the output in a jpg file, which was successful
